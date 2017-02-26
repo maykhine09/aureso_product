9af31bc91d82a248f03eab377ca461a743812573
May Khine <maykhine@nexlabs.co>
2017-02-26 22:50:31 +0630

    move to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  namespace :api do
+    namespace :v1 do
+      resources :products, :only => [:create]
+      get 'products/new', to: 'products#new'
+    end
+  end
 
-  resources :products, :only => [:create]
-  get '/products/new', to: 'products#new'
 end
